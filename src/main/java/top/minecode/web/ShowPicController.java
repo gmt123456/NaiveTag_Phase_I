acce83d6e8b4229a394a3f8b19c070558b2fb665
iznauy <zy05160516@126.com>
2018-03-22 19:01:44 +0800

    add controller to control the showing of pictures in index

---
+++ src/main/java/top/minecode/web/ShowPicController.java
@@ -0,0 +1,44 @@
+package top.minecode.web;
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on 2018/3/22.
+ * Description:
+ *
+ * @author iznauy
+ */
+@Repository
+public class ShowPicController {
+
+    @RequestMapping("/dataList")
+    @ResponseBody
+    public String getPicList(HttpServletRequest request) throws Exception {
+        HttpSession session = request.getSession();
+        if (session == null)
+            return;
+
+        int index = (Integer) request.getAttribute("index");
+        int count = (Integer) request.getAttribute("count");
+
+        List<String> pathList = (List<String>)session.getAttribute("picList");
+        String suffix = (String) session.getAttribute("path");
+
+        List<String> result = new ArrayList<String>();
+        for (int i = index; i < index + count; i++)
+            result.add(suffix + "data/" + pathList.get(i));
+
+        Gson gson = new Gson();
+        return gson.toJson(result);
+    }
+
+}
