b69a6e7284d25a79f8f9bff359beebf877ea2758
iznauy <zy05160516@126.com>
2018-03-23 20:08:54 +0800

    add task controller

---
+++ src/main/java/top/minecode/web/TaskController.java
@@ -0,0 +1,29 @@
+package top.minecode.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created on 2018/3/23.
+ * Description:
+ *
+ * @author iznauy
+ */
+
+@RestController
+public class TaskController {
+
+    @RequestMapping("/task")
+    public @ResponseBody String getPicList(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (session == null)
+            return null;
+        String suffix = (String) session.getAttribute("path");
+        return suffix + "task.json";
+    }
+
+}
